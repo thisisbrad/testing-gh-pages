2adca29fb20a9989589ff15746bf757da6b28e0b
Brad Beltowski <bradbeltowski@gmail.com>
2020-08-05 16:16:10 -0400

    Updates

---
+++ precache-manifest.d06499569f0f76b63b6e05291c9354e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db6ae78e119457adcafda0a0d96b592",
+    "url": "/github.io/portfolio-V/index.html"
+  },
+  {
+    "revision": "195d29adcc3dc63a1454",
+    "url": "/github.io/portfolio-V/static/css/main.3518268e.chunk.css"
+  },
+  {
+    "revision": "c9f12b8d16605065f089",
+    "url": "/github.io/portfolio-V/static/js/2.25ded2f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/portfolio-V/static/js/2.25ded2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195d29adcc3dc63a1454",
+    "url": "/github.io/portfolio-V/static/js/main.f4004c8e.chunk.js"
+  },
+  {
+    "revision": "d181392a29d33dd0d933",
+    "url": "/github.io/portfolio-V/static/js/runtime-main.d63e8ad0.js"
+  },
+  {
+    "revision": "2dba8ab70adbc7b288558d4dd211d706",
+    "url": "/github.io/portfolio-V/static/media/13.2dba8ab7.jpg"
+  },
+  {
+    "revision": "9b247305eb01cd62040ae29b3bb2ff70",
+    "url": "/github.io/portfolio-V/static/media/john.9b247305.jpeg"
+  }
+]);
